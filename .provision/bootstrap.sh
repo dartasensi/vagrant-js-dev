08518eaf51c21ddc1459d2ba2c5642a51387e23b
davik3000 <davik3000@users.noreply.github.com>
2017-06-27 16:51:48 +0200

    Improved provision script

--- .provision/bootstrap.sh
+++ .provision/bootstrap.sh
@@ -1,16 +1,26 @@
 #!/usr/bin/env bash
 
 # update the package db
-sudo apt-get update
+sudo apt-get update -q
 
+#
 # node.js 6.x
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-sudo apt-get install -y nodejs
+sudo apt-get update -q && sudo apt-get install -y nodejs
+# optional
 sudo apt-get install -y build-essential
 
+#
+# yarn
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get update -q && sudo apt-get install -y yarn
+
+##
 ## php, zip and git
 #sudo apt-get -y install php5 php5-cli php5-fpm zip unzip git
 
+##
 ## nginx
 #sudo apt-get -y install nginx
 #sudo service nginx start
@@ -21,6 +31,7 @@ sudo apt-get install -y build-essential
 #sudo ln -s /etc/nginx/sites-available/site.conf /etc/nginx/sites-enabled/site.conf
 #sudo service nginx restart
 
+##
 ## mysql
 #export DEBIAN_FRONTEND=noninteractive
 #echo "mysql-server mysql-server/root_password password root" | sudo debconf-set-selections
@@ -33,10 +44,13 @@ sudo apt-get install -y build-essential
 #DB_NAME="testDB"
 #mysql -u root -p"root" -e "CREATE DATABASE ${DB_NAME};"
 
+##
 ## install Python and verify Python
 # sudo apt-get -y install python3 python3-pip
 # python3 --version
 
+##
+## other
 ## clean /var/www
 #sudo rm -Rf /var/www
 ## symlink /var/www => /vagrant

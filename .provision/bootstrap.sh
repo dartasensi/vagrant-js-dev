e904d82ec30f6da6fce4106719ad0d6454822e76
davik3000 <davik3000@users.noreply.github.com>
2017-06-28 16:16:14 +0200

    Added manual proxy configuration for npm (in case it doesn't resolve it automatically with env vars)

--- .provision/bootstrap.sh
+++ .provision/bootstrap.sh
@@ -6,29 +6,40 @@
 
 #
 # utilities
+echo "> installing utilities"
 sudo apt-get update -qq && sudo apt-get install -y zip unzip git
-#
 # dev
 sudo apt-get update -qq && sudo apt-get install -y make gcc g++
 
 #
 # node.js 6.x
+echo "> installing Node.js"
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get update -qq && sudo apt-get install -y nodejs
 # optional: build-essential
+echo "> installing build-essential"
 sudo apt-get install -y build-essential
 #
 # optional: node-pre-gyp
+if [ -n "${HTTP_PROXY}" ] ; then
+  # apply proxy settings, if available
+  echo "> manually applying proxy settings to npm"
+  sudo npm config set proxy ${HTTP_PROXY} -g
+  sudo npm config set https-proxy ${HTTP_PROXY} -g
+fi
+echo "> installing node-pre-gyp through npm"
 sudo npm install node-pre-gyp -g
 
 #
 # yarn
+echo "> installing yarn"
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt-get update -qq && sudo apt-get install -y yarn
 
 #
 # sqlite3
+echo "> installing sqlite3 (libsqlite3-dev)"
 sudo apt-get update -qq && sudo apt-get install -y libsqlite3-dev sqlite3
 
 ##
